b39960889da4b0ec7aafe41c4650173cd28eb34f
roshangangule <roshangangule@gmail.com>
2023-08-03 16:20:06 +0530

    first commit

---
+++ src/main/java/com/betterreads/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.betterreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+}
